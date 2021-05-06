2d52867adabb98e91873a53955672497e67ca15b
Jean Cherin <jv.cherin@gmail.com>
2021-05-06 22:59:32 +0200

    added visual skeleton

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Input, InputGroup, InputGroupAddon, Button, ListGroup, ListGroupItem, Navbar, NavbarText, Nav, NavItem, NavLink } from 'reactstrap'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar style={{ width: 600 }} color="light" light expand="md">
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="https://github.com/JeanCherin">My GitHub</NavLink>
+          </NavItem>
+        </Nav>
+        <NavbarText>A To-Do App by Jean Cherin</NavbarText>
+      </Navbar>
+      <div>
+        <InputGroup style={{ width: 400 }}>
+          <Input placeholder="Name of to-do list" />
+          <InputGroupAddon addonType="append"><Button>Add</Button></InputGroupAddon>
+        </InputGroup>
+        <ListGroup style={{ width: 200 }}>
+          <ListGroupItem>To-Do List 1</ListGroupItem>
+          <ListGroupItem>To-Do List 2</ListGroupItem>
+          <ListGroupItem>To-Do List 3</ListGroupItem>
+        </ListGroup>
+      </div>
+      <div>
+        <InputGroup style={{ width: 400 }}>
+          <Input placeholder="Add Task" />
+          <InputGroupAddon addonType="append"><Button>Add</Button></InputGroupAddon>
+        </InputGroup>
+        <ListGroup style={{ width: 200 }}>
+          <ListGroupItem>Task 1</ListGroupItem>
+          <ListGroupItem>Task 2</ListGroupItem>
+          <ListGroupItem>Task 3</ListGroupItem>
+        </ListGroup>
+      </div>
     </div>
   );
 }
